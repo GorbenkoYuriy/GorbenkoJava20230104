f0b341b8c025cbd545bd1d8fb045a45808a15599
GorbenkoYuriy <gorbenko.u79+gh1@gmail.com>
2023-01-08 00:44:11 +0200

    BurgerCommit

---
+++ BURGER/BURGER.java
@@ -0,0 +1,22 @@
+public class BURGER {
+
+        String bun;
+        String meat;
+        String cheese;
+        String greenery;
+        String mayonnaise;
+    }
+
+    class burgernormal extends BURGER {
+        String burgernormal;
+
+        {
+            System.out.println("burger normal" + "bun" + "meat" + "cheese" + "greenery" + "mayonnaise");
+            {
+
+                System.out.println("в состав бургера водят следующие ингридиенты: булока, мясо, сыр, зелень, маойнез");
+            }
+
+        }
+    }
+}
