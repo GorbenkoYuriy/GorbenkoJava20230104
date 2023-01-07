694e7ec4793444abbc60059a6f2c15c338654ced
GorbenkoYuriy <gorbenko.u79+gh1@gmail.com>
2023-01-08 01:12:02 +0200

    BurgerCommit

--- GorbenkoJava20230104/src/BURGER.java
+++ GorbenkoJava20230104/src/BURGER.java
@@ -1,40 +1,43 @@
 public class BURGER {
+    public static void main(String[] args) {
 
-    String bun;
-    String meat;
-    String cheese;
-    String greenery;
-    String mayonnaise;
-}
-
-class burgernormal extends BURGER {
-    String burgernormal;
-
-    {
-        System.out.println("bun" + "meat" + "cheese" + "greenery" + "mayonnaise");
-        System.out.println("в состав бургера входят следующие ингридиенты: булочка, мясо, сыр, зелень, маойнез");
+        String булочка;
+        String мясо;
+        String сир;
+        String зелень;
+        String майонез;
     }
 
-    {
-        class burgerdietary extends BURGER {
-            String burgerdietary;
+    static class burgernormal extends BURGER {
+        String burgernormal;
 
-            {
-                System.out.println("bun" + "meat" + "cheese" + "greenery");
-                System.out.println("в состав диетического бургера входят следующие ингридиенты: булочка, мясо, сыр, зелень");
-            }
+        static {
+            System.out.println("булочка" + "м'ясо" + "сир" + "зелень" + "майонез");
+            System.out.println("до складу бургера входять наступні інгридієнти: булочка, м'ясо, сир, зелень, маойнез");
+        }
 
-            {
-                class burgerdoublemeat extends BURGER {
-                    String burgerdoublemeat;
+        static {
+            class burgerdietary extends BURGER {
+                String burgerdietary;
 
-                    {
-                        System.out.println("bun" + "doublemeat" + "cheese" + "greenery" + "mayonnaise");
-                        System.out.println("в состав диетического бургера входят следующие ингридиенты: булочка, двойное мясо, сыр, зелень, майонез");
+                static {
+                    System.out.println("булочка" + "м'ясо" + "сир" + "зелень");
+                    System.out.println("до складу бургера входять наступні інгридієнти: булочка, м'ясо, сир, зелень");
+                }
+
+                static {
+                    class burgerdoublemeat extends BURGER {
+                        String burgerdoublemeat;
+
+                        static {
+                            System.out.println("булочка" + "подвійне м'ясо" + "сир" + "зелень" + "майонез");
+                            System.out.println("до складу бургера входять наступні інгридієнти: булочка, подвійне м'ясо, сир, зелень, маойнез");
+                        }
                     }
                 }
             }
         }
+
     }
 }
 
